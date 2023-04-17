c5979f4edb380bde81126e0f23e2d43d7d910745
GeirlanthArchivest <100598359+GeirlanthArchivest@users.noreply.github.com>
2023-04-17 14:55:34 +0100

    added basic firing

---
+++ Squirrel Wars - Unity/Assets/Scripts/Launcher.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    [SerializeField] Transform ProjectilePrefab;
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] LineRenderer lineRenderer;
+
+    [SerializeField] float launchForce = 1.5f;
+    [SerializeField] float trajectoryTimeStep = 0.05f;
+    [SerializeField] int trajectoryStepCount = 15;
+
+    Vector2 velocity, startMousePos, currentMousePos;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            currentMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            velocity = (startMousePos = currentMousePos) * launchForce;
+
+            DrawTrajectory();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            FireProjectile();
+        }
+    }
+
+    void DrawTrajectory()
+    {
+        Vector3[] positions = new Vector3[trajectoryStepCount];
+        for(int  i = 0; i < trajectoryStepCount; i++)
+        {
+            float t = i * trajectoryTimeStep;
+            Vector3 pos = (Vector2)spawnPoint.position + velocity * t + 0.5f * Physics2D.gravity * t * t;
+
+            positions[i] = pos;
+        }
+
+        lineRenderer.positionCount = trajectoryStepCount;
+        lineRenderer.SetPositions(positions);
+    }
+
+    void FireProjectile()
+    {
+        Transform pr = Instantiate(ProjectilePrefab, spawnPoint.position, Quaternion.identity);
+
+        pr.GetComponent<Rigidbody2D>().velocity = velocity;
+    }
+}
