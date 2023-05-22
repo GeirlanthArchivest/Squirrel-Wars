2932a638515d1455d1a25081e43d4bbd7d6ab4bf
Eighthsum9798 <1633236@fife.ac.uk>
2023-05-22 15:33:36 +0100

    added assets, changed script for projectile

--- Squirrel Wars - Unity/Assets/Scripts/Projectile.cs
+++ Squirrel Wars - Unity/Assets/Scripts/Projectile.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-
+    //variable storing the lifetime of an object
     public float life = 3;
 
     void Awake()
-    {
+    {   //destroy the projectile after reaching value of life
         Destroy(gameObject, life);
     }
 
@@ -16,6 +16,30 @@ public class Projectile : MonoBehaviour
     // Start is called before the first frame update
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+       
+        // this checks what object type the projectile has collided with
+        if(collision.gameObject.CompareTag("Wall") ||
+           collision.gameObject.CompareTag("Enemy") ||
+           collision.gameObject.CompareTag("Character"))
+        {
+            // destroys the object the projectile collided with
+            Destroy(collision.gameObject);
+            
+            //destroys theprojectile
+            Destroy(gameObject);
+
+
+        }
+        //else statement in case projectile collides with something it isnt checking for
+        else
+        {
+            //destroys projectile
+            Destroy(gameObject);
+        }
+
+        
+        
+
+        
     }
 }
